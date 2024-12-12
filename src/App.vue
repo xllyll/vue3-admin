a10064eef5d19fa91af810aaddb0eabdae841e7d
xllyll <757271398@qq.com>
2024-12-12 15:28:48 +0800

    init project Signed-off-by: xllyll <757271398@qq.com>

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
